cce24829aca9671f4f072bdb143a380b73f2cd2e
Daniel Morrison <daniemo2@cisco.com>
2018-09-11 22:21:56 +0100

    Converts images, zips and downloads

--- public/app.js
+++ public/app.js
@@ -1,14 +1,25 @@
 angular.module("myApp", []).controller("myCtrl", function ($scope, $http) {
     $scope.background = "#ffffff";
     $scope.accent = "#049fd9";
+    $scope.generating = false;
 
     $scope.submit = function () {
+        $scope.generating = true;
         console.log("Submit");
         var image = document.getElementById("image");
         image = image.outerHTML.replace('{{ background }}', $scope.background);
         image = image.replace(/{{ accent }}/g, $scope.accent);
 
         console.log(image);
+
+        $http.post('/api/convert', {'image': image}).then(function(res) {
+            console.log(res);
+            $scope.generating = false;
+
+            window.location.href = '/api/download';
+        }, function(err) {
+            console.log(err);
+        });
     }
 
 });
--- server.js
+++ server.js
@@ -3,10 +3,16 @@
 // set up ========================
 var express = require('express');
 var app = express(); // create our app w/ express
+var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
+const icongen = require('icon-gen');
+var fs = require('fs');
+var zipFolder = require('zip-folder');
+
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({
     'extended': 'true'
 })); // parse application/x-www-form-urlencoded
@@ -18,13 +24,51 @@ app.use(methodOverride());
 
 // listen (start app with node server.js) ======================================
 var port = process.env.PORT || 8080;
-app.listen(port);
-console.log("app listening on " + port)
 
-app.get('/api/test', function (req, res) {
-    res.send('hi');
+app.get('/api/download', function(req, res) {
+    res.download('./icons.zip');
+})
+
+app.post('/api/convert', function (req, res) {
+    console.log("Converting");
+    console.log(req.body.image);
+
+    fs.writeFile("src/image.svg", req.body.image, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+
+        const options = {
+            report: true
+        };
+    
+        icongen('./src/image.svg', './dist', options)
+            .then((results) => {
+                console.log(results)
+                zipFolder('./dist', './icons.zip', function(err) {
+                    if(err) {
+                        console.log('oh no!', err);
+                        res.send('error');
+                    } else {
+                        console.log('EXCELLENT');
+                        res.download('icons.zip');
+                    }
+                });
+
+                
+            })
+            .catch((err) => {
+                console.error(err)
+                res.send('error');
+            });
+    }); 
 });
 
 app.get('*', function(req, res) {
     res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
+
+app.listen(port);
+console.log("app listening on " + port)
